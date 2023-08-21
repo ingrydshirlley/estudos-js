70c5b23fa19c58d67c35797b07e2c2acd4310445
ingryd <ingryddasilva7@gmail.com>
2023-08-21 18:40:09 -0300

    oi

--- modulo_react/ingryd/src/App.jsx
+++ modulo_react/ingryd/src/App.jsx
@@ -1,14 +1,7 @@
-import Card from "./Card";
+import styles from "./index.module.css";
 
 function App() {
-  return (
-    <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  )
+  return <h1 className={styles.title}>Hello</h1>
 }
 
 export default App; 
