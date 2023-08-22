543a1b0ec8ee95a5ca887ddbf963a24842115a31
ingryd <ingryddasilva7@gmail.com>
2023-08-22 08:47:37 -0300

    ˜oi˜

--- modulo_react/ingryd/src/App.jsx
+++ modulo_react/ingryd/src/App.jsx
@@ -1,7 +1,42 @@
-import styles from "./index.module.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return <h1 className={styles.title}>Hello</h1>
+
+  const [repositorio, setRepositorio] = useState([])
+  useEffect(() => {
+
+    async function carregaRepositorios() {
+
+      const resposta = await fetch(
+        "https://api.github.com/users/ingrydshirlley/repos"
+      )
+
+      const repositorios = await resposta.json()
+      setRepositorio(repositorios)
+    }
+
+    carregaRepositorios()
+  }, [])
+
+  return (
+    <div>
+      <h1>retorno da funcao</h1>
+      <ul>
+        {repositorio.map((repositorio) => (
+          <div>
+            <li key={repositorio.id}>{repositorio.name}
+              <div>
+                <span>linguagem: {repositorio.language} </span>
+                <img src={repositorio.owner.avatar_url} alt=""></img>
+              </div>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
 }
+
 
 export default App; 
